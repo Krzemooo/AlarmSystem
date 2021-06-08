5b6ccba37f1aaaa36f382786ffa7a9fb1e5ed2b1
Dominik Krzeminski <dominik@mukisolution.com>
2021-06-08 19:19:02 +0200

    Add project files.

---
+++ AlarmSystem.Core/DBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlarmSystem.Core
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options)
+        {
+
+        }
+    }
+}
